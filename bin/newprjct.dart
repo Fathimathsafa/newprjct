6017e07ba88b90b2b1de9f7e31ce5111a9503fd1
fathimathsafa <safahaneefa01@gmail.com>
2023-10-27 09:31:16 +0530

    Initial commit

---
+++ bin/newprjct.dart
@@ -0,0 +1,5 @@
+import 'package:newprjct/newprjct.dart' as newprjct;
+
+void main(List<String> arguments) {
+  print('Hello world: ${newprjct.calculate()}!');
+}
---
+++ test/newprjct_test.dart
@@ -0,0 +1,8 @@
+import 'package:newprjct/newprjct.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
